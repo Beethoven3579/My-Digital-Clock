4ab86b93afd7390cb390c61ef85612d577a6d6c8
Beethoven3579 <75546304+Beethoven3579@users.noreply.github.com>
2021-04-03 13:39:46 -0400

    Clock app finished!

--- app.js
+++ app.js
@@ -1,32 +1,64 @@
-const monthName = ["January", "February", "March", "April", "May", "June", "July", 
-"August", "September", "October", "November", "December"];
-const dayName = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-const fullDate = new Date();
 
-document.getElementById('weekday').innerHTML = dayName[fullDate.getDay() - 1];
-document.getElementById('month').innerHTML = monthName[fullDate.getMonth()];
-document.getElementById('date').innerHTML = fullDate.getDate() + ",";
-document.getElementById('year').innerHTML = fullDate.getFullYear();
+function date() {
+    const fullDate = new Date();
+    const dateDisplay = `${dayName[fullDate.getDay() - 1]}
+    ${monthName[fullDate.getMonth()]}
+    ${fullDate.getDate()}, ${fullDate.getFullYear()}`;
 
-const clockContainer = document.getElementById('clock');
-
-function addZeros(num) {
-    if (num < 10){
-        num = "0" + num;
-    }
-    return num;
+    const dateDiv = document.getElementById('date');
+    dateDiv.textContent = dateDisplay;
 }
+
+const monthName = 
+["January", 
+"February", 
+"March", 
+"April",
+"May",
+"June", 
+"July", 
+"August", 
+"September", 
+"October", 
+"November", 
+"December"];
+
+const dayName = 
+["Monday", 
+"Tuesday", 
+"Wednesday", 
+"Thursday", 
+"Friday", 
+"Saturday", 
+"Sunday"];
+
 
 function clock() {
-    const date = new Date();
-    const hours = addZeros(date.getHours());
-    const minutes = addZeros(date.getMinutes());
-    const seconds = addZeros(date.getSeconds());
-    const time = `${hours} : ${minutes} : ${seconds}`;
+    const time = new Date();
+    const hours = renderTwelveHourClock(time.getHours());
+    const minutes = addZeros(time.getMinutes());
+    const seconds = addZeros(time.getSeconds());
+    const isAm = hours > 12 || hours === 0;
+    let amPm = isAm ? 'AM' : 'PM';
+    const timeDisplay = `${hours} : ${minutes} : ${seconds} ${amPm}`;
     
-    clockContainer.innerHTML = time;
+    const clockContainer = document.getElementById('clock');
+    clockContainer.textContent = timeDisplay;
 }
 
-clock();
+function addZeros(number) {
+    return number < 10 ? '0' + number : number;
+}
 
-setInterval(clock, 1000);
+function renderTwelveHourClock(hour) {
+    hour = hour >= 13 ? hour - 12 : hour;
+    hour = hour === 0 ? hour + 12 : hour;
+    return hour;
+}
+
+
+setInterval(() => {
+    clock();
+    date();
+}, 1000);
+
